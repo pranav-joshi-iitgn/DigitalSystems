e5d1463804321080169258f3e92dbcacc8f533b9
Pranav Joshi <pranav.joshi@iitgn.ac.in>
2024-03-14 03:21:47 +0530

    Update DS_Project.v

--- DS_Project.v
+++ DS_Project.v
@@ -1,6 +1,21 @@
-`timescale 1ns / 1ps
-module read();
-reg [15:0]w[0:790585];
+`timescale 1s / 1fs
+module ram(clk,index,write,data_in,data_out);
+input clk,write;
+input [32:0]index;
+input [15:0]data_in;
+output reg [15:0]data_out;
+reg [15:0]data[0:790387];
+always @(posedge clk)
+begin
+    if(write)
+        data[index] <= data_in;
+    else
+        data_out <= data[index];
+end
+endmodule
+
+`timescale 1s/1fs
+module ram_t();
 /*
 Sizes and shapes of weights(+ biases stacked on top as birst row) (fro w0 to w7)
 314384 (400, 784)
@@ -14,7 +29,8 @@ Maximum bits required :  14.999516
 Total numbers needed to store :  790586)
 */
 reg [15:0]captured_data;
-integer i,scan_file;
+integer scan_file;
+integer i;
 integer data_file;
 initial
 begin
@@ -29,14 +45,26 @@ begin
     $display("read");
   end
 end
+reg clk,write;
+reg [32:0]index;
+wire [15:0]data_out;
+reg [15:0]data_in;
+ram uut(clk,index,write,data_in,data_out);
+initial begin
+    clk=0; write=1;
+    index= 0;
+    forever #1 clk = ~clk;
+end
 initial begin
     // next d_in numbers are for the bias and then from there, there are d_out clusters of d_in numbers for each row of weight matrix.
     // I am putting all that in a single array.
-    for(i=0;i<790586;i=i+1)
+    for(i=0;i<790388;i=i+1)
     begin
         scan_file = $fscanf(data_file, "%d\n", captured_data); 
-        w[i] = captured_data;
+        index = i;
+        data_in = captured_data;
+        #2;
     end
-    #10;
+    $finish;
 end
 endmodule
